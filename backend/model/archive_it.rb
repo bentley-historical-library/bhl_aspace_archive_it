8fab289955c7f518f856c361fdabfad856194de8
djpillen@umich.edu <djpillen@umich.edu>
2018-04-11 16:35:14 -0400

    remove creator parsing logic from the identification of MARC candidates

--- backend/model/archive_it.rb
+++ backend/model/archive_it.rb
@@ -1,24 +1,7 @@
 class ArchiveIt
 
     def self.get_marc_candidates
-        DB.open do |db|
-            # find site-level archival objects that have creators that do not show up in any other non web archives resources
-            creator_enum_id = db[:enumeration].filter(:name=>'linked_agent_role').join(:enumeration_value, :enumeration_id => Sequel.qualify(:enumeration, :id)).where(:value => 'creator').all[0][:id]
-            # Get an array of archival object ids for site-level archival objects
-            site_ids = ArchivalObject.filter(:other_level=>'seed').exclude(:parent_id => nil).map(:parent_id).uniq
-            # Get arrays of agent ids that are linked to site-level archival objects
-            person_creators = db[:linked_agents_rlshp].filter({:archival_object_id => site_ids, :role_id => creator_enum_id}).exclude(:agent_person_id => nil).map(:agent_person_id).uniq
-            corporate_creators = db[:linked_agents_rlshp].filter({:archival_object_id => site_ids, :role_id => creator_enum_id}).exclude(:agent_corporate_entity_id => nil).map(:agent_corporate_entity_id).uniq
-            # Get arrays of agent ids that are linked to an existing resource
-            resource_linked_people = db[:linked_agents_rlshp].filter(:agent_person_id => person_creators).exclude(:resource_id => nil).map(:agent_person_id).uniq
-            resource_linked_corps = db[:linked_agents_rlshp].filter(:agent_corporate_entity_id => corporate_creators).exclude(:resource_id => nil).map(:agent_corporate_entity_id).uniq
-            # Get arrays of agents that are linked as creators from site-level archival objects but which are not associated with any resources
-            unique_people = person_creators - resource_linked_people
-            unique_corps = corporate_creators - resource_linked_corps
-            # Get an array of archival object ids that are site-level archival objects and have a creator that is not associated with any resources
-            unique_sites = db[:linked_agents_rlshp].filter(:archival_object_id => site_ids).where(:agent_person_id => unique_people).or(:agent_corporate_entity_id => unique_corps).map(:archival_object_id).uniq
-            unique_sites
-        end
+        site_ids = ArchivalObject.filter(:other_level => 'seed').exclude(:parent_id => nil).map(:parent_id).uniq
     end
 
     def self.get_wayback_links_and_earliest_capture(site_id)
